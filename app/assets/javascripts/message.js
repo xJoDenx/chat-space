e2e9dbb67e78982f8b083ec65c04fd41712b2c18
xJoDenx <weider.in.jelly.61@gmail.com>
2019-09-21 20:00:26 +0900

    edit_automation_update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(document).on('turbolinks:load', function() {
 
   function buildHTML(message){
     var img = message.image.url ? message.image.url : `` ;
-    var html = `<div class="right-main__message">
+    var html = `<div class="right-main__message" data-id= "${message.id}" >
                   <div class="right-main__message__user">
                     ${message.user_name}
                   </div>
@@ -14,7 +14,7 @@ $(document).on('turbolinks:load', function() {
                     ${message.content}
                   </div>
                   <div class= "right-main__message__image">
-                    <img src='${img}'>
+                    <img src='${img}' class= 'message__image'>
                   </div>
                 </div>`
     return html;
@@ -43,4 +43,34 @@ $(document).on('turbolinks:load', function() {
       alert('error');
     })
   })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.right-main__message:last').data('id');
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: 'api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      messages.forEach(function(message){
+        //メッセージが入ったHTMLを取得
+        var insertHTML = buildHTML(message);
+        //メッセージを追加
+        $('.right-main').append(insertHTML);
+        $('.right-main').animate({ scrollTop: $('.right-main')[0].scrollHeight});
+      });
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+    setInterval(reloadMessages, 5000);
 });
